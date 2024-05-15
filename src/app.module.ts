d5282921e1a2719754357d3102ab494f866d5373
Joshua Dela Cruz <joshua_delacruz@questronix.com.ph>
2024-05-15 21:42:42 +0800

    [SETUP] project model

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { DatabaseService } from './database/database.service';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-    imports: [DatabaseModule, AuthModule, ConfigModule.forRoot()],
+    imports: [DatabaseModule, AuthModule, ConfigModule.forRoot(), ProjectsModule],
     providers: [DatabaseService],
 })
 export class AppModule {}
